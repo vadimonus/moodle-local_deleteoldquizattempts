4c404a7137654f20a2d4a2c3d59cc7c7e997b98b
Vadim Dvorovenko <Vadimon@mail.ru>
2024-01-14 23:16:56 +0700

    Remove unneeded phpcs skip

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -22,8 +22,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// phpcs:disable PSR2.Methods.MethodDeclaration.Underscore
-
 namespace local_deleteoldquizattempts\privacy;
 
 use core_privacy\local\metadata\null_provider;
